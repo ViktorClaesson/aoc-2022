2cbc2f96e90ae6ef30bbf16eae868635098acc16
Viktor Claesson <claesson.viktor@gmail.com>
2022-12-07 12:35:11 +0100

    day 6 part 2

--- src/day_6.ts
+++ src/day_6.ts
@@ -2,14 +2,17 @@ import fs from "fs";
 
 // methods
 
-function answer(data: string[]): number {
+function answer(data: string[], package_size: number): number {
   return [...Array(data.length).keys()]
-    .map((idx) => data.slice(Math.max(0, idx - 4), idx))
-    .findIndex((value) => value.length === 4 && new Set(value).size === 4);
+    .map((idx) => data.slice(Math.max(0, idx - package_size), idx))
+    .findIndex(
+      (value) =>
+        value.length === package_size && new Set(value).size === package_size
+    );
 }
 
 // solve
 
 const data: string[] = fs.readFileSync("inputs/day_6.txt", "utf8").split("");
-console.log(`answer 1: ${answer(data)}`);
-console.log(`answer 2: ${0}`);
+console.log(`answer 1: ${answer(data, 4)}`);
+console.log(`answer 2: ${answer(data, 14)}`);
