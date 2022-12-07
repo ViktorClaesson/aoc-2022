8a0a8f836111cee651556d780bc0902a2ff78bdf
Viktor Claesson <claesson.viktor@gmail.com>
2022-12-07 13:44:58 +0100

    update day_X to have separate answer functions

--- src/day_X.ts
+++ src/day_X.ts
@@ -6,12 +6,16 @@ class Foo {}
 
 // methods
 
-function answer(): number {
+function answer_1(): number {
+  return 42;
+}
+
+function answer_2(): number {
   return 42;
 }
 
 // solve
 
 const data: string = fs.readFileSync("inputs/day_X.txt", "utf8");
-console.log(`answer 1: ${answer()}`);
-console.log(`answer 2: ${answer()}`);
+console.log(`answer 1: ${answer_1()}`);
+console.log(`answer 2: ${answer_2()}`);
