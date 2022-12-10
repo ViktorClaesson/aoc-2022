fafbb3016b282c36c85634d6caf46ede7e27775e
Viktor Claesson <claesson.viktor@gmail.com>
2022-12-10 15:51:41 +0100

    day 10 part 1, added comments

--- src/day10.ts
+++ src/day10.ts
@@ -5,12 +5,15 @@ import fs from "fs";
 function answerPartOne(instructions: string[]): number {
   return instructions
     .flatMap((instruction) =>
-      instruction.startsWith("addx") ? [0, +instruction.split(" ")[1]] : [0]
-    )
-    .reduce((acc, val) => acc.concat(acc[acc.length - 1] + val), [1, 1])
-    .map((val, index) => val * index)
-    .filter((_, index) => (index - 20) % 40 === 0)
-    .reduce((acc, val) => acc + val, 0);
+      instruction.startsWith("addx") ? ["noop", instruction] : [instruction]
+    ) // padd addx instructions with noop instruction before it
+    .map((instruction) =>
+      instruction.startsWith("addx") ? +instruction.split(" ")[1] : 0
+    ) // convert instructions to the amount to be added (0 for noop)
+    .reduce((acc, val) => acc.concat(acc[acc.length - 1] + val), [1, 1]) // convert array into new array where each index is the cycle, and the value the register value at the beginning of the cycle
+    .map((val, index) => val * index) // multiple each register value by the cycle
+    .filter((_, index) => (index - 20) % 40 === 0) // only keep cycle 20 and then every 40th after that
+    .reduce((acc, val) => acc + val, 0); // sum the cycles
 }
 
 function answerPartTwo(): number {
