71d62bd4153e5be6597f1dfc4bc92b2eb807bb7c
Viktor Claesson <claesson.viktor@gmail.com>
2022-12-11 15:08:06 +0100

    day 11 part 2

--- src/day11.ts
+++ src/day11.ts
@@ -3,7 +3,7 @@
 class Monkey {
   items: number[];
   operation: (item: number) => number;
-  test: (item: number) => boolean;
+  testNumber: number;
   positiveTestMonkeyIndex: number;
   negativeTestMonkeyIndex: number;
   inspectCounter: number = 0;
@@ -11,25 +11,25 @@ class Monkey {
   constructor(
     startingItems: number[],
     operation: (item: number) => number,
-    test: (item: number) => boolean,
+    testNumber: number,
     positiveTestMonkeyIndex: number,
     negativeTestMonkeyIndex: number
   ) {
     this.items = startingItems;
     this.operation = operation;
-    this.test = test;
+    this.testNumber = testNumber;
     this.positiveTestMonkeyIndex = positiveTestMonkeyIndex;
     this.negativeTestMonkeyIndex = negativeTestMonkeyIndex;
   }
 
-  doTurn(monkeys: Monkey[]) {
+  doTurn(monkeys: Monkey[], worryFunction: (item: number) => number) {
     this.inspectCounter += this.items.length;
     this.items
       .map(this.operation)
-      .map((item) => Math.floor(item / 3))
+      .map(worryFunction)
       .forEach((item) =>
         monkeys[
-          this.test(item)
+          item % this.testNumber === 0
             ? this.positiveTestMonkeyIndex
             : this.negativeTestMonkeyIndex
         ].items.push(item)
@@ -40,9 +40,26 @@ class Monkey {
 
 // methods
 
-function answerPartOne(monkeys: Monkey[]): number {
-  for (let i = 0; i < 20; i++) {
-    monkeys.forEach((monkey) => monkey.doTurn(monkeys));
+function initMonkeys(): Monkey[] {
+  return [
+    new Monkey([56, 52, 58, 96, 70, 75, 72], (item) => item * 17, 11, 2, 3),
+    new Monkey([75, 58, 86, 80, 55, 81], (item) => item + 7, 3, 6, 5),
+    new Monkey([73, 68, 73, 90], (item) => item ** 2, 5, 1, 7),
+    new Monkey([72, 89, 55, 51, 59], (item) => item + 1, 7, 2, 7),
+    new Monkey([76, 76, 91], (item) => item * 3, 19, 0, 3),
+    new Monkey([88], (item) => item + 4, 2, 6, 4),
+    new Monkey([64, 63, 56, 50, 77, 55, 55, 86], (item) => item + 8, 13, 4, 0),
+    new Monkey([79, 58], (item) => item + 6, 17, 1, 5),
+  ];
+}
+
+function answer(
+  monkeys: Monkey[],
+  rounds: number,
+  worryFunction: (item: number) => number
+): number {
+  for (let i = 0; i < rounds; i++) {
+    monkeys.forEach((monkey) => monkey.doTurn(monkeys, worryFunction));
   }
 
   return monkeys
@@ -52,70 +69,23 @@ function answerPartOne(monkeys: Monkey[]): number {
     .reduce((a, b) => a * b, 1);
 }
 
-function answerPartTwo(): number {
-  return 42;
+function answerPartOne(monkeys: Monkey[]): number {
+  return answer(monkeys, 20, (item) => Math.floor(item / 3));
+}
+
+function answerPartTwo(monkeys: Monkey[]): number {
+  return answer(
+    monkeys,
+    10000,
+    (item: number) =>
+      item %
+      monkeys
+        .map((monkey) => monkey.testNumber)
+        .reduce((acc, testNumber) => acc * testNumber, 1)
+  );
 }
 
 // solve
 
-const monkeys: Monkey[] = [
-  new Monkey( // 0
-    [56, 52, 58, 96, 70, 75, 72],
-    (item) => item * 17,
-    (item) => item % 11 === 0,
-    2,
-    3
-  ),
-  new Monkey( // 1
-    [75, 58, 86, 80, 55, 81],
-    (item) => item + 7,
-    (item) => item % 3 === 0,
-    6,
-    5
-  ),
-  new Monkey( // 2
-    [73, 68, 73, 90],
-    (item) => item ** 2,
-    (item) => item % 5 === 0,
-    1,
-    7
-  ),
-  new Monkey( // 3
-    [72, 89, 55, 51, 59],
-    (item) => item + 1,
-    (item) => item % 7 === 0,
-    2,
-    7
-  ),
-  new Monkey( // 4
-    [76, 76, 91],
-    (item) => item * 3,
-    (item) => item % 19 === 0,
-    0,
-    3
-  ),
-  new Monkey( // 5
-    [88],
-    (item) => item + 4,
-    (item) => item % 2 === 0,
-    6,
-    4
-  ),
-  new Monkey( // 6
-    [64, 63, 56, 50, 77, 55, 55, 86],
-    (item) => item + 8,
-    (item) => item % 13 === 0,
-    4,
-    0
-  ),
-  new Monkey( // 7
-    [79, 58],
-    (item) => item + 6,
-    (item) => item % 17 === 0,
-    1,
-    5
-  ),
-];
-
-console.log(`Answer part 1: ${answerPartOne(monkeys)}`);
-console.log(`Answer part 2: ${answerPartTwo()}`);
+console.log(`Answer part 1: ${answerPartOne(initMonkeys())}`);
+console.log(`Answer part 2: ${answerPartTwo(initMonkeys())}`);
