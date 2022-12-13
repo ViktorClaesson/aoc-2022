a9e25cb0e254390a55699e5b4fedc0b1da3fb1d9
Viktor Claesson <claesson.viktor@gmail.com>
2022-12-13 22:48:09 +0100

    day 13 part 2

--- src/day13.ts
+++ src/day13.ts
@@ -1,4 +1,6 @@
 import fs from "fs";
+import { equals } from "./utils/common";
+import { id1 } from "./utils/debug";
 
 // data structures && init functions
 
@@ -6,7 +8,10 @@ type Packet = (Packet | number)[];
 
 // solution methods
 
-function checkPackets([packet1, packet2]: Packet[]): boolean | undefined {
+function checkPacketsRec(
+  packet1: Packet,
+  packet2: Packet
+): boolean | undefined {
   if (packet1.length === 0 && packet2.length === 0) {
     return undefined;
   }
@@ -25,49 +30,63 @@ function checkPackets([packet1, packet2]: Packet[]): boolean | undefined {
   if (typeof packet1Value === "number") {
     if (typeof packet2Value === "number") {
       if (packet1Value === packet2Value) {
-        return checkPackets([packet1.slice(1), packet2.slice(1)]);
+        return checkPacketsRec(packet1.slice(1), packet2.slice(1));
       } else {
         return packet1Value < packet2Value;
       }
     }
 
-    return checkPackets([[packet1Value], packet2Value]);
+    return checkPacketsRec([packet1Value], packet2Value);
   }
 
   if (typeof packet2Value === "number") {
-    return checkPackets([packet1Value, [packet2Value]]);
+    return checkPacketsRec(packet1Value, [packet2Value]);
   }
 
-  const answer: boolean | undefined = checkPackets([
-    packet1Value,
-    packet2Value,
-  ]);
-  if (answer === undefined) {
-    if (packet1.length === 1 && packet2.length === 1) {
+  const answer = checkPacketsRec(packet1Value, packet2Value);
+  return answer === undefined
+    ? checkPacketsRec(packet1.slice(1), packet2.slice(1))
+    : answer;
+}
+
+function checkPackets(packet1: Packet, packet2: Packet): boolean {
+  let answer;
+  while ((answer = checkPacketsRec(packet1, packet2)) === undefined) {
+    packet1 = packet1.slice(1);
+    packet2 = packet2.slice(1);
+    if (packet1.length === 0 && packet2.length === 0) {
       return true;
     }
-    return checkPackets([packet1.slice(1), packet2.slice(1)]);
   }
   return answer;
 }
 
-function answerPartOne(pairs: string[][]): number {
-  return pairs
-    .map(([list1, list2]) => [eval(list1), eval(list2)])
-    .map((packets, index) => (checkPackets(packets) ? index + 1 : 0))
+function answerPartOne(packetPairs: [Packet, Packet][]): number {
+  return packetPairs
+    .map((packets, index) => (checkPackets(...packets) ? index + 1 : 0))
     .reduce((acc, val) => acc + val, 0);
 }
 
-function answerPartTwo(): number {
-  return 42;
+function answerPartTwo(packets: Packet[]): number {
+  return [[[2]], [[6]], ...packets]
+    .sort((packet1, packet2) => -2 * +checkPackets(packet1, packet2) + 1)
+    .map((packet, index) =>
+      equals(packet, [[2]]) ? index + 1 : equals(packet, [[6]]) ? index + 1 : 1
+    )
+    .reduce((acc, val) => acc * val, 1);
 }
 
 // solve
 
-const data: string[][] = fs
-  .readFileSync("inputs/day13.txt", "utf8")
+const data: string = fs.readFileSync("inputs/day13.txt", "utf8");
+const packetPairs: [Packet, Packet][] = data
   .split("\n\n")
-  .map((pair) => pair.split("\n"));
+  .map((pair) => pair.split("\n"))
+  .map(([packet1, packet2]) => [eval(packet1), eval(packet2)]);
+const packets: Packet[] = data
+  .split("\n")
+  .filter((line) => line.length > 0)
+  .map((line) => eval(line));
 
-console.log(`Answer part 1: ${answerPartOne(data)}`);
-console.log(`Answer part 2: ${answerPartTwo()}`);
+console.log(`Answer part 1: ${answerPartOne(packetPairs)}`);
+console.log(`Answer part 2: ${answerPartTwo(packets)}`);
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -21,3 +21,17 @@ export function rollingChunks<T>(arr: T[], chunkSize: number): T[][] {
     arr.slice(index, index + chunkSize)
   );
 }
+
+export function equals(a: any, b: any): boolean {
+  if (typeof a !== typeof b) {
+    return false;
+  }
+
+  if (Array.isArray(a)) {
+    return (
+      a.length === b.length && a.every((val, index) => equals(val, b[index]))
+    );
+  } else {
+    return a === b;
+  }
+}
